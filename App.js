6f7f5f74f8aa9ea568980cc787a8446107900fd7
sqrmyy <dn41.ra20adtdjw@gmail.com>
2020-11-11 08:41:41 +0900

    fix App.js

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  Button,
+} from 'react-native';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const schema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, '3文字以上で入力してください')
+    .max(20, '20文字以内で入力してください')
+    .required('氏名を入力してください'),
+});
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: '100%',
+    padding: 24,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  form: {
+    width: '100%',
+  },
 });
+
+export default class App extends Component {
+  onSubmit = async (values, actions) => {
+    // データ送信
+  };
+
+  render() {
+    return (
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.form}>
+          <Formik
+            initialValues={{
+              name: '',
+            }}
+            validateOnMount
+            validationSchema={schema}
+            onSubmit={this.onSubmit}
+          >
+            {({
+              handleSubmit,
+              handleChange,
+              handleBlur,
+              isValid,
+              isSubmitting,
+              values,
+              errors,
+              touched,
+            }) => (
+              <>
+                <View>
+                  {errors.name && touched.name ? (
+                    <Text>{errors.name}</Text>
+                  ) : null}
+                  <TextInput
+                    value={values.name}
+                    onChangeText={handleChange('name')}
+                    onBlur={handleBlur('name')}
+                    placeholder='氏名を入力してください'
+                  />
+                </View>
+                <Button
+                  title='Submit'
+                  onPress={handleSubmit}
+                  disabled={!isValid || isSubmitting}
+                />
+              </>
+            )}
+          </Formik>
+        </View>
+      </ScrollView>
+    );
+  }
+}
